38dc791a90d208e950969539d483e6dc51abee10
Megumi Ieki <megumi.ieki@rakuten.com>
2022-05-20 18:30:10 +0900

    Add test for RootHeader

---
+++ src/components/__tests__/RootHeader.spec.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import RootHeader from "../organisms/RootHeader.vue";
+
+describe("RootHeader", () => {
+  it("renders properly", () => {
+    const wrapper = mount(RootHeader, { props: { title: "Title" } });
+    expect(wrapper.text()).toContain("Title");
+  });
+});
