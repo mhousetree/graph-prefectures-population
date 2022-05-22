ccee89deb75694f3265a2a1fe7cb21df52b1fce7
Megumi Ieki <megumi.ieki@rakuten.com>
2022-05-22 22:44:08 +0900

    Improve api config by using axios.create

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,29 +1,23 @@
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api/v1/",
+  headers: {
+    "X-API-KEY": import.meta.env.VITE_API_KEY,
+  },
+});
+
 export const getPrefectures = async () => {
-  const response = await axios.get(
-    import.meta.env.VITE_API_URL + "/api/v1/prefectures",
-    {
-      headers: {
-        "X-API-KEY": import.meta.env.VITE_API_KEY,
-      },
-    }
-  );
+  const response = await instance.get("prefectures");
   return response.data.result;
 };
 
 export const getTotalPopulation = async (prefCode) => {
-  const response = await axios.get(
-    import.meta.env.VITE_API_URL + "/api/v1/population/composition/perYear",
-    {
-      params: {
-        prefCode: prefCode,
-        cityCode: "-",
-      },
-      headers: {
-        "X-API-KEY": import.meta.env.VITE_API_KEY,
-      },
-    }
-  );
+  const response = await instance.get("population/composition/perYear", {
+    params: {
+      prefCode: prefCode,
+      cityCode: "-",
+    },
+  });
   return response.data.result.data.find((v) => v.label === "総人口").data;
 };
